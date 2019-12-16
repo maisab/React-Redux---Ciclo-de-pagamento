48804c620893d32eaabd4130668d399e49c95ad4
maisab <maaisa.barreto@gmail.com>
2019-12-16 15:24:07 -0300

    configuração fronted

--- my-money-app/backend/src/config/routes.js
+++ my-money-app/backend/src/config/routes.js
@@ -1,12 +1,12 @@
 const express = require('express')
 
 module.exports = function(server) {
-    
-    //url base para todas as rotas
+
+    // Definir URL base para todas as rotas 
     const router = express.Router()
     server.use('/api', router)
 
-    //rotas relacionadas ao ciclo de pagamento
-    const  BillingCycle = require('../api/billingCycle/billingCycleService')
+    // Rotas de Ciclo de Pagamento 
+    const BillingCycle = require('../api/billingCycle/billingCycleService')
     BillingCycle.register(router, '/billingCycles')
 }
